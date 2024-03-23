507a4c3ffa7a34556cb2234575f8bc6866e37dbf
Salah-Eddine Harchiche <saharchi@e3r8p5.1337.ma>
2024-03-23 22:56:50 +0000

    edit

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 
 double map(double x, double in_min, double in_max, double out_min, double out_max) 
 {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  return ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
 }
 
 
