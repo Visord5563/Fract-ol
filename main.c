cd6846b00e84d2d63904dbae21795a0377446730
Salah-Eddine Harchiche <saharchi@e3r10p16.1337.ma>
2024-03-23 22:50:04 +0000

    edit

--- main.c
+++ main.c
@@ -19,10 +19,11 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-double map(double x, double in_min, double in_max, double out_min, double out_max)
+double map(double x, double in_min, double in_max, double out_min, double out_max) 
 {
-	return ((x - in_min) * (out_max - out_min) / ((in_max - in_min) + out_min));
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+
 
 int	main(void)
 {
@@ -34,32 +35,34 @@ int	main(void)
 	win = mlx_new_window(mlx,800,800,"fract-ol");
 	img.img = mlx_new_image(mlx,800,800);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	double a ; 
-	double b  ;
-	double x ;
-	double y = -1;
-	int iteration ;
-	int max_iteration =80;
-	while(++y < 800)
+	double a; 
+	double b;
+	double xs;
+	double ys = -1;
+	int iteration;
+	int max_iteration =100;
+
+	while(++ys < 800)
 	{
-		x = -1;
-		while(++x < 800)
+		xs = -1;
+		while(++xs < 800)
 		{
 			iteration = 0;
-			double xtemp  =0;
-			b = map(y, 0, 800, -2, 2);
-			a = map(x, 0,800, -2, 2);
-			while(((x*x) + (y*y) <= 4) && (iteration < max_iteration))
+			double xtemp ;
+			double x =0; 
+			double y =0;
+			b = map(ys, 0, 800, -2, 2);
+			a = map(xs, 0,800, -2, 2);
+			while(((x*x) + (y*y) <= 4) && (++iteration < max_iteration))
 			{
 				xtemp = ((x*x) - (y*y)) + a;
-				y = (2* x * y) + b;
+				y = (2* x * y) - b;
 				x = xtemp;
-				iteration++;
 			}
 		if (iteration == max_iteration)
-			my_mlx_pixel_put(&img, x ,y , 0x00FF);
+			my_mlx_pixel_put(&img, xs ,ys , 0x000000000);
 		else
-			my_mlx_pixel_put(&img, x ,y , 0x00FFF346);
+			my_mlx_pixel_put(&img, xs ,ys , 0xFFFFFFF);
 		}
 	}
 	mlx_put_image_to_window(mlx,win,img.img,0,0);
