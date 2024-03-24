9365445f5bf4d11534f7e00772485fbaddf5a0a8
Salah-Eddine Harchiche <salaheddineharchiche5563@gmail.com>
2024-03-24 02:08:39 +0000

    edit

--- mandelbrod.c
+++ mandelbrod.c
@@ -6,7 +6,7 @@
 /*   By: saharchi <saharchi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 01:17:27 by saharchi          #+#    #+#             */
-/*   Updated: 2024/03/24 02:03:17 by saharchi         ###   ########.fr       */
+/*   Updated: 2024/03/24 02:08:36 by saharchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,7 @@ void ft_julia(double y, double x, t_data *img)
 		z = xtemp;
 	}
 	if (iteration == max_iteration)
-		my_mlx_pixel_put(img, x ,y ,iteration * 0x00000<<16);
+		my_mlx_pixel_put(img, x ,y ,iteration * 0x00000ff<<16);
 	else
 		my_mlx_pixel_put(img, x ,y ,iteration * 0xff00000);
 }
